74bf73a88d7e4889ca2e9dcbc3cb3471dd06510e
Nathaniel Struselis <Nathaniel.Struselis@hotmail.co.uk>
2025-01-06 21:29:25 +0000

    Added sample MAUI app (.net 9).
    I've changed the Application title and ID accordingly.
    
    I've also made sure that it runs on Android and Windows. Since it's a
    sample app you might think that's ridiculous, but this is MAUI we're
    dealing with... The sample app has frequently not worked between .net6
    and .net8...

---
+++ src/BLEarringController/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BLEarringController
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
