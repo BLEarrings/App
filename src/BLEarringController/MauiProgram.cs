7f3e4e910ac9cb77d06de85449f8c3365851e0f3
Nathaniel Struselis <Nathaniel.Struselis@hotmail.co.uk>
2025-01-10 01:22:04 +0000

    Added CommunityToolkit.Maui NuGet package.

--- src/BLEarringController/MauiProgram.cs
+++ src/BLEarringController/MauiProgram.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace BLEarringController
 {
     public static class MauiProgram
     {
+        #region Methods
+
+        #region Public Static
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -21,5 +27,9 @@ namespace BLEarringController
 
             return builder.Build();
         }
+
+        #endregion
+
+        #endregion
     }
 }
