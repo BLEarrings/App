4b678285f0a251b308362a5d2f4be93111845c19
Nathaniel Struselis <Nathaniel.Struselis@hotmail.co.uk>
2025-01-10 01:18:12 +0000

    Renamed MainPage to HomePage.

---
+++ src/BLEarringController/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BLEarringController.ViewModels
+{
+    public sealed class HomePageViewModel : ViewModelBase
+    {
+        #region Properties
+
+        #region Public
+
+        /// <summary>
+        /// The subtitle to display underneath the <see cref="Title"/>.
+        /// </summary>
+        public string Subtitle => "Wow... this is a very empty home page! Must be a placeholder...";
+
+        /// <summary>
+        /// The title to display at the top of the page.
+        /// </summary>
+        public string Title => "Home Page";
+
+        #endregion
+
+        #endregion
+    }
+}
