ff9be3c85d15990a25366dde40e85359ae7661ad
Nathaniel Struselis <Nathaniel.Struselis@hotmail.co.uk>
2025-01-18 15:23:09 +0000

    Register all views with the dependency injection container using Scrutor. Fixes #27.
    All views will derive from ContentPage, so Scrutor can be used to regsiter all ContentPages as themself with a transient lifetime.

--- src/BLEarringController/MauiProgram.cs
+++ src/BLEarringController/MauiProgram.cs
@@ -25,6 +25,19 @@ namespace BLEarringController
     		builder.Logging.AddDebug();
 #endif
 
+                // ----------------------------- Views and ViewModels -----------------------------
+
+                // All Views will derive from ContentPage, so filter out all classes that derive
+                // from ContentPage from the list of all public, none-abstract classes within the
+                // assembly.
+                .AddClasses(classes => classes.AssignableTo<ContentPage>())
+                    // Register each matching type as itself, as they will only be referred to by
+                    // their class name to distinguish the different views.
+                    .AsSelf()
+                    // Register the views with a transient lifetime, as they should be
+                    // re-instantiated each time they are referenced.
+                    .WithTransientLifetime()
+
             return builder.Build();
         }
 
